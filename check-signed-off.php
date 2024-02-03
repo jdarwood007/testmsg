28241ef092361b3513b6650f2e66a6d088643e71
jdarwood007 <unmonitored+github@sleepycode.com>
2024-02-03 07:35:41 -0800

    a

--- check-signed-off.php
+++ check-signed-off.php
@@ -11,196 +11,173 @@
  * @version 3.0
  */
 
-// Debug stuff.
+/*
+ * Enable debugging of the script.
+ */
 define('DEBUG_MODE', true);
+
+/*
+ * All the strings we search for.
+ */
+define('SIGN_OFF_STRINGS', ['Signed-off-by:', 'Signed by']);
 
 try
 {
-	debugPrint("--- DEBUG MSGS START ---");
+	writeDebug('# Check Sign off');
 
-	// First, lets do a basic test.  This is non GPG signed commits.
-	$signedoff = find_signed_off();
+	$result = parseCommit('HEAD');
 
-	// Now Try to test for the GPG if we don't have a message.
-	if (empty($signedoff)) {
-		debugPrint("Standard Sign off missing, checking GPG");
-		$signedoff = find_gpg();
+	writeDebug('---', '### Result');
+
+	if ($result !== '') {
+		writeDebug('Found a valid sign off in commit: ' . $result);
+	} else {
+		fwrite(STDERR, 'No valid sign off was found in your commit, please sign off commits.');
 	}
 
-	// Nothing yet?  Lets ask your parents.
-	if (empty($signedoff)) {
-		debugPrint("No sign off found, checking the parents.");
-		$signedoff = find_signed_off_parents();
+	// Send debugging information out.
+	if ((bool) DEBUG_MODE === true) {
+		writeDebug('---', '### Debugging information', '```');		
+		print_r($GLOBALS['debugger']);
+		writeDebug('```');
 	}
 
-	debugPrint("--- DEBUG MSGS END ---");
-
-	// Nothing?  Well darn.
-	if (empty($signedoff)) {
-		fwrite(STDERR, 'Error: No valid Sign-off found.  Please Sign your commits.');
-		exit(0);
-	}
-
-	debugPrint('Valid signed off found');
+	exit(0);
 }
 catch (Exception $e)
 {
-	fwrite(STDERR, $e->getMessage() . 'STDERR');
-	fwrite(STDOUT, $e->getMessage()) . 'STDOUT';
-	print($e->getMessage()) . 'PRINT';
-	exit(0);
+	fwrite(STDERR, $e->getMessage());
+
+	// This is a fatal error, exit so CI stops.
+	exit(1);
 }
 
-// Find a commit by Signed Off
-function find_signed_off(string $commit = 'HEAD', array $childs = [], int $level = 0): bool
+function parseCommit(string $hash, int $level = 0): string
 {
-	if (empty($commit)) {
-		debugPrint('Commit is empty');
-		exit(0);
-	}
+	global $debugger;
+	static $regex = '';
 
-	// Where we are at.
-	debugPrint('Attempting to find signed off on commit ' . $commit);
+	$hash = trim($hash ?? '');
 
-	// To many recrusions here.
-	if ($level > 10)
-	{
-		debugPrint('Recusion limit exceeded on find_signed_off');
-		return false;
-	}
-
-	// What string tests should we look for?
-	$stringTests = ['Signed-off-by:', 'Signed by'];
-
-	// Get message data and clean it up, should only need the last line.
-	$message = trim(shell_exec('git show -s --format=%B ' . $commit));
-	$lines = explode("\n", trim(str_replace("\r", "\n", $message)));
-	$lastLine = $lines[count($lines) - 1];
-
-	// Debug info.
-	debugPrint('Testing line "' . $lastLine . '"');
-
-	// loop through each test and find one.
-	$result = false;
-	foreach ($stringTests as $testedString)
-	{
-		debugPrint('Testing "' . $testedString . '"');
-
-		$result = stripos($lastLine, $testedString);
-
-		// We got a result.
-		if ($result !== false)
-		{
-			debugPrint('Found "' . $testedString . '"');
-			break;
-		}
-	}
-
-	// Debugger.
-	$debugMsgs = [
-		'raw body' => '"' . rtrim(shell_exec('git show -s --format=%B ' . $commit)) . '"',
-		'body' => '"' . rtrim(shell_exec('git show -s --format=%b ' . $commit)) . '"',
-		'commit notes' => '"' . rtrim(shell_exec('git show -s --format=%N ' . $commit)) . '"',
-		'ref names' => '"' . rtrim(shell_exec('git show -s --format=%d ' . $commit)) . '"',
-		'commit hash' => '"' . rtrim(shell_exec('git show -s --format=%H ' . $commit)) . '"',
-		'tree hash' => '"' . rtrim(shell_exec('git show -s --format=%T ' . $commit)) . '"',
-		'parent hash' => '"' . rtrim(shell_exec('git show -s --format=%P ' . $commit)) . '"',
-		'result' => '"' . $result . '"',
-		'testedString' => '"' . $testedString . '"',
-	];
-	debugPrint('Commit ' . $commit . ' at time ' . time() . ": " . rtrim(print_r($debugMsgs, true)));
-
-
-	// No result and found a merge? Lets go deeper.
-	if ($result === false && preg_match('~Merge ([A-Za-z0-9]{40}) into ([A-Za-z0-9]{40})~i', $lastLine, $merges))
-	{
-		debugPrint('Found Merge, attempting to get more parent commit: ' . $merges[1]);
-
-		return find_signed_off($merges[1], array_merge(array($merges[1]), $childs), ++$level);
-	}
-
-	return $result !== false;
-}
-
-// Find a commit by GPG
-function find_gpg(string $commit = 'HEAD', array $childs = []): bool
-{
-	if (empty($commit)) {
-		debugPrint('Commit is empty');
-		exit(0);
-	}
-
-	debugPrint('Attempting to Find GPG on commit ' . $commit);
-	$result = false;
-
-	// Get verify commit data.
-	$message = trim(shell_exec('git verify-commit ' . $commit . ' --raw 2>&1') ?? '');
-
-	// Should we actually test for gpg results?  Perhaps, but it seems doing that with travis may fail since it has no way to verify a GPG signature from GitHub.  GitHub should have prevented a bad GPG from making a commit to a authors repository and could be trusted in most cases it seems.
-	if (!empty($message)) {
-		debugPrint('We found a valid GPG signature. message lenth: '  . strlen($message) . ']');	
-		$result = true;
+	// No hash found, abort.
+	if (strlen($hash) === 0) {
+		writeDebug('## ERROR', 'Received invalid git hash');
+		return '';
 	} else {
-		debugPrint('No GPG results returned, no signature detected.');	
+		writeDebug('## Checking: ' . $hash);
 	}
 
-	// Debugger.
-	$debugMsgs = [
-		// Raw body.
-		'verify-commit' => '"' . rtrim(shell_exec('git verify-commit ' . $commit . ' --raw -v 2>&1') ?? '') . '"',
-		// Result.
-		'result' => '"' . $result . '"',
-		// Last tested string, or the correct string.
-		'message' => '"' . $message . '"',
+	// Build the regex up.
+	if (empty($regex)) {
+		$regex = '~(' . implode('|', array_map('preg_quote', (array) SIGN_OFF_STRINGS)) . ')~i';
+	}
+
+	// For the debugger.
+	$debugger[$hash] ??= [];
+	$data = &$debugger[$hash];
+	$data = [
+		'msg' => gitCmd('log --graph --abbrev-commit --decorate --first-parent --no-merges -n1 --show-signature ' . $hash),
+		'hash' => gitCmd('show -s --format=%H ' . $hash),
+		'tree' => gitCmd('show -s --format=%T ' . $hash),
+		'parent' => gitCmd('show -s --format=%P ' . $hash),
 	];
-	debugPrint('Commit ' . $commit . ' at time ' . time() . ": " . rtrim(print_r($debugMsgs, true)));
 
-	return $result;
-}
+	// First, try just the message.
+	$data['lines'] = explode("\n", trim(str_replace("\r", "\n", $data['msg'])));
+	$data['lastline'] = $data['lines'][count($data['lines']) - 1];
 
-// Looks at all the parents, and tries to find a signed off by somewhere.
-function find_signed_off_parents(string $commit = 'HEAD'): bool
-{
-	if (empty($commit)) {
-		debugPrint('Commit is empty');
-		exit(0);
+	if (preg_match($regex, $data['lastline'], $match)) {
+		writeDebug('*Found sign off in message*');
+		$data['matched'] = $match[1];
+
+		return $hash;
 	}
 
-	$commit = trim($commit);
-	$result = false;
+	// See if it contains a GPG signing.
+	if (preg_match('~^\| gpg:\s+using([^\r\n]+)~im', $data['msg'], $match)) {
+		writeDebug('*Found GPG header*');
+		$data['matched'] = $match[1];
 
-	debugPrint('Attempting to find parents on commit ' . $commit);
+		return $hash;
+	}
 
-	$parentsRaw = rtrim(shell_exec('git show -s --format=%P ' . $commit));
-	$parents = explode(' ', $parentsRaw);
+	/*
+	 * DO NOT CHECK MERGES
+	 * Merges may contain commits not from the author.
+	 * Merges may be a main branch merge or rebase.
+	 *
+	 * DO NOT CHECK PARENTS
+	 * The last commit by the author (non merge) is
+	 * the only commit that needs checked.
+	 *
+	 * This code is left in here for experimental purposes.
+	*/
 
-	// Test each one.
-	foreach ($parents as $parent)
-	{
-		$parent = trim($parent);
-		debugPrint('Testing parent(' . $commit. ') for commit ' . $parent . ' for signed off or GPG');
+	/*
+	// Do we have a merge?
+	if (preg_match('~Merge ([A-Za-z0-9]{40}) into ([A-Za-z0-9]{40})~i', $data['lastline'], $merge)) {
+		writeDebug('*Merge Header found, checking*');
 
-		// Basic tests.
-		$result = find_signed_off($parent);
-
-		// No, maybe it has a GPG parent.
-		if (empty($result)) {
-			$result = find_gpg($parent);
+		// Recrusion threshold.
+		if ($level > 10) {
+			writeDebug('*Recurusion limit reached and we still have more merges to check*');
+			return '';
 		}
-		
-		if ($result) {
+
+		$data['merge'] = $merge;
+
+		$result = parseCommit($merge[1], $level + 1);
+
+		if ($result !== '') {
+			writeDebug('*Merge check successfull*');
 			return $result;
 		}
 	}
 
-	// Lucked out.
-	return $result;
+	// Seek parents.
+	if ($data['parent'] !== '') {
+		writeDebug('*Checking parents*');
+		$data['parents'] = explode(' ', $data['parent']);
+
+		$result = '';
+		foreach ($data['parents'] as $parent_hash) {
+			writeDebug('*Parsing parent commit ' . $parent_hash . '*');
+			$result = parseCommit($parent_hash, $level + 1);
+
+			if ($result !== '') {
+				writeDebug('*Parent check successfull*');
+				return $result;
+			}
+		}
+	}
+	*/
+
+	writeDebug('*Compelted checks, no sign offs found*');
+	return '';
 }
 
-// Print a debug line
-function debugPrint($msg)
+/*
+ * Handles writing a debug message only when we have it enabled
+ */
+function writeDebug(...$msg): void
 {
-	if (DEBUG_MODE) {
-		echo $msg, "\n";
+	if ((bool) DEBUG_MODE === true) {
+		echo implode("\n", array_map('strval', (array) $msg)), "\n";
 	}
+}
+
+/*
+ * Shows just the stub
+ */
+function getStub(string $commit): string
+{
+	return substr($commit, 0, 7);
+}
+
+/*
+ * Clean up our exec process.
+ */
+function gitCmd(string $command, bool $no_pager = true): string
+{
+	return trim(shell_exec('git ' . ($no_pager ? ' --no-pager ' : '') . $command . ' 2>&1') ?? '');
 }
